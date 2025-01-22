0d26e9d2f44e024a85976375c3c9ba9ca1084136
computational mama <ambikajoshi@gmail.com>
2025-01-23 00:18:10 +0530

    shell script and more

--- utils/build_site.sh
+++ utils/build_site.sh
@@ -19,7 +19,7 @@ done
 
 if [[ $updated != "forced rebuild" ]]; then
         echo "Checking for update $now"
-        updated=$(git -C $repoDir pull origin main)
+        updated=$(sudo git -C $repoDir pull origin main)
 fi
 
 
@@ -34,16 +34,16 @@ else
         cd $repoDir
 
         echo "Dithering new images"
-        /usr/bin/python3 utils/dither_images.py -d $contentDir --colorize
+        sudo ./venv/bin/python utils/dither_images.py -d $contentDir --colorize
 
         echo "Generating site"
-        hugo -b $baseURL --destination $outputDir
+        sudo hugo -b $baseURL --destination $outputDir
 
         echo "Calculating page sizes"
-        /usr/bin/python3 utils/calculate_size.py --directory $outputDir --baseURL $baseURL
+        sudo ./venv/bin/python utils/calculate_size.py --directory $outputDir --baseURL $baseURL
 
         echo "Removing original media from" $outputDir
-        /usr/bin/python3 utils/clean_output.py --directory $outputDir
+        sudo ./venv/bin/python utils/clean_output.py --directory $outputDir
 
         after=`date`
         echo "Site regeneration started $now"
